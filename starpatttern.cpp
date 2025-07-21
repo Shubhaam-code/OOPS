e09a869dc18cc0124999194a474b45445af371b6
Subham Narayan <subhamnarayan11144@gmail.com>
2025-07-21 11:05:48 +0530

    shubham

--- starpatttern.cpp
+++ starpatttern.cpp
@@ -4,12 +4,11 @@ using namespace std;
 int main(){
 
     int row ,col;
-    for(row =1; row<=5;row++){
-        for(col=1;col<=5;col++){
-            char name ='a';
-            cout<<name<<" ";
-            name='a'+1;
-
+    for(row =1; row<=5;row++)
+    {
+        for(col=5;col>=row;col--)
+        {
+            cout<<"*"<<" ";
         }
         cout<<endl;
     }
