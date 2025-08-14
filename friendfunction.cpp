e54f786adbd42cb8741d14c0b3b1a02e4e75088d
Subham Narayan <subhamnarayan11144@gmail.com>
2025-08-14 11:08:41 +0530

    update

--- friendfunction.cpp
+++ friendfunction.cpp
@@ -1,17 +1,3 @@
 #include <iostream>
 using namespace std;
 
-class love{
-    private:
-    int a;
-    public:
-    love(){
-        a=25;
-    }
-
-    friend void display(love b );
-};
-
-void display( love b){
-    cout<<b.a;
-}
