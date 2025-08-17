27c5bc0fac370e8fe91c0af0b0e582f6ee6f3783
Subham Narayan <subhamnarayan11144@gmail.com>
2025-08-17 13:39:43 +0530

    update

---
+++ FUNCTION/friendclass.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class X{
+    int x;
+    public:
+    X(int a){
+        x=a;
+    }
+    friend class Y;
+};
+class Y{
+    public:
+    void show(X obj){
+        cout<<obj.x;
+    }
+};
+
+int main(){
+    X obj(5);
+    Y aa;
+    aa.show(obj);
+}
